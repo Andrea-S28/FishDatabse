4626bf97fa5f5c1dc9570dab58a33aa708135ff5
Ethan <umanae@mail.gvsu.edu>
2024-10-23 21:13:34 -0400

    rest of fish images

--- PycharmProjects/FishDatabase/Fishial_Recognition.py
+++ PycharmProjects/FishDatabase/Fishial_Recognition.py
@@ -39,7 +39,7 @@ val_transforms = Compose([
 ])
 
 # splits data into train and validate sizes
-train_samples, test_samples = int(.6 * len(dataset)), len(dataset) - int(.6 * len(dataset))
+train_samples, test_samples = int(.7 * len(dataset)), len(dataset) - int(.7 * len(dataset))
 # takes both samples and randomly sorts them
 train_dataset, val_dataset = torch.utils.data.random_split(dataset, lengths=[train_samples, test_samples])
 # makes sure each image is transformed to preset
@@ -116,10 +116,10 @@ def train(model, device, epochs, optimizer, loss_fn, batch_size, trainloader, va
         log_training['validation loss'].append(valid_loss_mean)
         log_training['validation accuracy'].append(valid_acc_mean)
 
-        print(f'Training Loss: {training_loss_mean}')
-        print(f'Training Accuracy: {training_acc_mean}')
-        print(f'Validation Loss: {valid_loss_mean}')
-        print(f'Validation Accuracy: {valid_acc_mean}')
+        print(f'Training Loss: {training_loss_mean:.2f}')
+        print(f'Training Accuracy: {training_acc_mean:.2f}')
+        print(f'Validation Loss: {valid_loss_mean:.2f}')
+        print(f'Validation Accuracy: {valid_acc_mean:.2f}')
 
     return log_training, model
 
@@ -153,6 +153,6 @@ def prediction(image_path, model, transform, device):
 
     return label
 
-image_path = './test_bass.jpg'
+image_path = './test.jpg'
 predicted_label = prediction(image_path, model, val_transforms, DEVICE)
 print(predicted_label)
