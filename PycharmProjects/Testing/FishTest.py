0eb0d251497ea2414e574babf0fb500393570a01
Ezekiel Turnbough <turnbou1@msu.edu>
2024-11-20 15:12:58 -0500

    Added the rest of the tests for the fish file.

--- PycharmProjects/Testing/FishTest.py
+++ PycharmProjects/Testing/FishTest.py
@@ -1,6 +1,7 @@
 from PycharmProjects.FishDatabase import Fish as f
 
 
+
 def test_add_fish_info():
     fish_id = f.add_fish_info("Marlin", "Clownfish", "Marlin is a orange saltwater clownfish with three stripes.")
     assert True == f.find_fish_exist(fish_id)
